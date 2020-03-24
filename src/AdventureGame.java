b1afa94a5b9d02f554d5d9dda55ff8c1116475c6
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-03-24 15:38:13 -0500

    pushing

--- src/AdventureGame.java
+++ src/AdventureGame.java
@@ -3,9 +3,16 @@ import java.util.Random;
 public class AdventureGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Wary from the day’s travels down many miles of bad road laden with brigands and ne’er-do-wells, you finally reach your destination:\n the quaint and run-down lakeside village of Fortune.\n\nThe desire to wet your whistle is overwhelming, the local tavern for meat, mead, and lodging is calling your name.\n\n  The tavern, like the rest of the village, has seen much better days and the lack of activity save for the burly innkeeper behind the bar is cause for concern,\n but you decide to sit regardless. Your posterior has barely hit the chair when a frothy mug is slammed down before you by the innkeeper.\n”What’s your name, traveler?\n\n");
+        String heroName = scanner.nextLine();
+
 
 //        Initial user query
-        System.out.println("Shall we play a game? (Y/N)");
+        System.out.println("Hail and well met, " + heroName);
+        System.out.println("You reach for a silver coin and slide it across the bar to the innkeeper.\n\“A bed, a meal, and mead for the night\n\n"
+//                “All well and good but you can save you coin in exchange for brave deeds done and high adventure. Do you fancy yourself an adventurer or are you just a traveler passing through?”\n\n" +
+//                "1 - “My blade already runs red with the blood of those on the road in who tried to deprive me of gold and goods — tell me more of this quest!”\n" +
+//                "2 - “I’m merely a traveling merchant — work and worry are all the adventure I can handle, count me out!\" \n");
         String userInput = scanner.next();
         boolean confirmation = userInput.equalsIgnoreCase("y");
             if (confirmation) {
@@ -36,6 +43,7 @@ public class AdventureGame {
                 System.out.printf("Your defensive skill has blocked %d point but %f got through!\n", heroDefense, totalDamage );
                 System.out.printf("Your current health is: %f\n", heroHealth - totalDamage);
             }
+
 
 
 
